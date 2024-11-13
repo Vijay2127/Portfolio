9eb183d9347b9aa6557e70d6f8f3239572157720
Vijay2127 <mvijaykumarpbt@gmail.com>
2024-11-13 21:35:11 +0530

    Resumepath

--- src/components/Slidebar.jsx
+++ src/components/Slidebar.jsx
@@ -8,7 +8,7 @@ const Slidebar = () => {
    
         <div className='w-[90px] border-r-[1px] h-screen fixed flex flex-col justify-around items-center
         mt-20'>
-            <a className='text-black -rotate-90 tracking-widest w-[130px] cursor-pointer hover:scale-105 transition-all ease-in-out hover:text-red-500 ]' href="/public/Vijay_Resume.pdf" download={"Vijay_Resume.pdf"}>Download CV</a>
+            <a className='text-black -rotate-90 tracking-widest w-[130px] cursor-pointer hover:scale-105 transition-all ease-in-out hover:text-red-500 ]' href="/Vijay_Resume.pdf" download="Vijay_Resume.pdf">Download CV</a>
             <div className='flex flex-col gap-7 mb-10'>
                 <a target='_blank' href="https://www.linkedin.com/in/vijayakumar-m-0276592b6/"><FaLinkedin className='cursor-pointer text-[20px] hover:scale-110 transition-all ease-in-out'/></a>
                 <a target='_blank' href="https://github.com/Vijay2127"><FaGithub className='cursor-pointer text-[20px] hover:scale-110 transition-all ease-in-out text-black'/></a>
